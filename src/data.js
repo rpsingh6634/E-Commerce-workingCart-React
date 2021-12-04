a611e9f7b9f580a2947e1afafd15445fd17b4d73
rpsingh6634 <rpsingh6634@gmail.com>
2021-12-04 13:13:01 +0530

    first commit

---
+++ src/data.js
@@ -0,0 +1,39 @@
+import img1 from './img/img1.jpg';
+import img2 from './img/img2.jpg';
+import img3 from './img/img3.jpg';
+import img4 from './img/img4.jpg';
+
+const data = {
+    productData:[
+        {
+           id: 1,
+           img: img1,
+           title: 'Bag',
+           desc: '',
+           price: 36,
+        },
+        {
+            id: 2,
+            img: img2,
+            title: 'Camera',
+            desc: '',
+            price: 56,
+         },
+         {
+            id: 3,
+            img: img3,
+            title: 'Watch',
+            desc: '',
+            price: 46,
+         },
+         {
+            id: 4,
+            img: img4,
+            title: 'Dell Laptop',
+            desc: '',
+            price: 566,
+         },
+    ],
+};
+
+export default data;
